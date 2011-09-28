e20f85719784691be473a99d728f321f370780ae
James Moger <james.moger@gitblit.com>
2011-09-28 10:10:55 -0400

    Reset build identifiers to SNAPSHOT.

--- src/com/gitblit/Constants.java
+++ src/com/gitblit/Constants.java
@@ -29,11 +29,11 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "0.6.0";
+	public static final String VERSION = "0.6.1-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "2011-09-27";
+	public static final String VERSION_DATE = "PENDING";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
