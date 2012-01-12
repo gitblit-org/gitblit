04627e5fff260c88120d3f7f01dba052d04c47da
James Moger <james.moger@gitblit.com>
2012-01-11 20:04:01 -0500

    Fixed missing icon resource for the manager (issue 40)

--- src/com/gitblit/Constants.java
+++ src/com/gitblit/Constants.java
@@ -29,11 +29,11 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "0.8.0";
+	public static final String VERSION = "0.8.1-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "2012-01-11";
+	public static final String VERSION_DATE = "PENDING";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
