eec3ef21b9a4503c11f21c292b843adbdc3ee7fc
James Moger <james.moger@gitblit.com>
2013-01-15 23:13:59 -0500

    Prepare 1.2.1 release

--- src/com/gitblit/Constants.java
+++ src/com/gitblit/Constants.java
@@ -34,11 +34,11 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "1.2.1-SNAPSHOT";
+	public static final String VERSION = "1.2.1";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "PENDING";
+	public static final String VERSION_DATE = "2012-01-15";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
