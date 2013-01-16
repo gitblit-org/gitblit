1176328299cd04cbba9568358feb84c1888977f5
James Moger <james.moger@gitblit.com>
2013-01-15 23:16:51 -0500

    Reset build identifiers for next release

--- src/com/gitblit/Constants.java
+++ src/com/gitblit/Constants.java
@@ -34,11 +34,11 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.3.0-SNAPSHOT";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION_DATE = "2012-01-15";
+	public static final String VERSION_DATE = "PENDING";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
