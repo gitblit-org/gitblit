960b00983e50879c03b0a3077988518273bc4dbe
saheba <mail-at-saheba-dot-net>
2013-05-06 11:30:17 +0200

    finished frontend for additive, ordered usage of multiple user services at the same time

--- src/main/java/com/gitblit/wicket/panels/UserServiceConfigPanel.java
+++ src/main/java/com/gitblit/wicket/panels/UserServiceConfigPanel.java
@@ -1,85 +1,175 @@
 package com.gitblit.wicket.panels;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.HiddenField;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.markup.repeater.util.ModelIteratorAdapter;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 
 public class UserServiceConfigPanel extends BasePanel {
-    private class GroupMapToggle extends AjaxEventBehavior {
-    	private Panel panelUnderControl;
-		private CheckBox controller;
-    	
-    	public GroupMapToggle(CheckBox controller, Panel panelUnderControl) {
-    		super("onchange");
-    		this.panelUnderControl=panelUnderControl;
-    		this.controller=controller;
-    	}
-		protected void onUpdate(AjaxRequestTarget target) {
-			// enable/disable permissions panel based on access restriction
-			System.out.println("SHB update");
-			updateVisibility();
-		}
-		
-		protected void updateVisibility() {
-			boolean isSelected = false;
-			
-			if (this.controller != null && this.controller.getModelObject() != null) {
-				isSelected = this.controller.getModelObject().booleanValue();
-			}
-			System.out.println("SHB "+isSelected+" input: "+this.controller.getInput()+" value: "+this.controller.getValue());
-			System.out.println("SHB "+this.controller.toString());
-			if (isSelected) {
-				this.panelUnderControl.setVisible(true);
-				this.panelUnderControl.setEnabled(true);
-			} else {
-				this.panelUnderControl.setVisible(false);
-				this.panelUnderControl.setEnabled(false);
-			}
-//			this.panelUnderControl.render();
-		}
-		@Override
-		protected void onEvent(AjaxRequestTarget target) {
-			// TODO Auto-generated method stub
-			System.out.println("SHB on event");
-			updateVisibility();
-		}
-    }
-	public UserServiceConfigPanel(String wicketId, String title) {
+	
+	private boolean connectorIsVisible = false, mappingIsVisible = false;
+	public static final String CSS_INVISIBLE_UGM = "invisibleUgmForm";
+	public static final String CSS_VISIBLE_UGM = "visibleUgmForm";
+
+	public UserServiceConfigPanel(String wicketId, String title,
+			Set<String> connectorParams, Set<String> systemRoles,
+			final List<RoleMapping> alreadyDefinedRoleModels) {
 		super(wicketId);
+
+		Label titleLabel = new Label("titleLabel", title);
+		Label connectorTitle = new Label("ugmConnectorDetailsTitle",
+				"Connector Settings");
+		Label mappingTitle = new Label("ugmMappingRulesTitle",
+				"Mapping Rules Form");
+
+		StatelessForm<Void> form = new StatelessForm<Void>("ugmPanelForm");
+
+		Button connectorButton = new Button("connector");
+
+		Button mappingButton = new Button("mapping");
+	
+		form.add(titleLabel);
+		form.add(connectorButton);
+		form.add(mappingButton);
+		form.add(connectorTitle);
+
+		ArrayList<String> params = new ArrayList<String>();
+		for (String string : connectorParams) {
+			params.add(string.replace(wicketId + ".", ""));
+		}
+		ListView<String> listview = new ListView<String>("listview", params) {
+			protected void populateItem(ListItem<String> item) {
+				item.add(new HiddenField("paramField", item.getModel()));
+				Label label = new Label("label", item.getModelObject().replace(
+						"synchronize", "sync"));
+				item.add(label);
+				IModel<String> valueModel = new IModel<String>() {
+
+					private static final long serialVersionUID = 1L;
+					private String value = "";
+
+					@Override
+					public void detach() {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public String getObject() {
+						return this.value;
+					}
+
+					@Override
+					public void setObject(String object) {
+						this.value = object;
+
+					}
+				};
+				item.add(new TextField("valueField", valueModel));
+			}
+		};
+		form.add(listview);
+		final ArrayList<String> roles = new ArrayList<String>();
+		for (String string : systemRoles) {
+			roles.add(string);
+		}
+		form.add(mappingTitle);
+		RefreshingView<RoleMapping> listRolesview = new RefreshingView<RoleMapping>(
+				"listRolesview") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected Iterator<IModel<RoleMapping>> getItemModels() {
+				return new ModelIteratorAdapter<RoleMapping>(
+						alreadyDefinedRoleModels.iterator()) {
+					@Override
+					protected IModel<RoleMapping> model(RoleMapping mapping) {
+						return new CompoundPropertyModel<RoleMapping>(mapping);
+					}
+				};
+			}
+
+			@Override
+			protected void populateItem(Item<RoleMapping> item) {
+				IModel<String> remoteRoleModel = new IModel<String>() {
+
+					private static final long serialVersionUID = 1L;
+					private String value = "";
+
+					@Override
+					public void detach() {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public String getObject() {
+						return this.value;
+					}
+
+					@Override
+					public void setObject(String object) {
+						this.value = object;
+
+					}
+				};
+				remoteRoleModel.setObject(((RoleMapping) item.getModelObject()).getRemoteRole()); 
+				item.add(new TextField("remoteRole", remoteRoleModel));
+				
+				
+				
+				DropDownChoice<String> systemRolesChoice = new DropDownChoice<String>(
+						"systemRoleSelection", roles,new DropDownTranslationRenderer());
+				item.add(systemRolesChoice);
+			}
+		};
 		
-//		Label titleLabel = new Label("titleLabel",title);
-//		CheckBox authUsersBox = new CheckBox("authenticateUsers");
-//		CheckBox mapGroupsBox = new CheckBox("mapGroups");
-//		TextField remoteGroupToSystemAdminField = new TextField("remoteGroupToSystemAdmin");
-//		TextField remoteGroupToRepoAdminField = new TextField("remoteGroupToRepoAdmin");
-//		TextField remoteGroupToRepoCreatorField = new TextField("remoteGroupToRepoCreator");
-//		CheckBox groupsForPermissionManagementBox = new CheckBox("groupsForPermissionManagement");
-////		Label prefixLabel = new Label("prefixLabel","prefix");
-//		TextField prefixField = new TextField("prefix"); 
-////		CheckGroup options = new CheckGroup("userServiceOptions");
-////		options.add(mapGroupsBox);
-//		
-////		MapGroupsPanel mapGroupsPanel = new MapGroupsPanel("mapGroupsSection");
-////		GroupMapToggle groupMapToggle = new GroupMapToggle(mapGroupsBox,mapGroupsPanel);
-////		groupMapToggle.updateVisibility();
-////		mapGroupsBox.add(groupMapToggle);
-//		
-//		add(titleLabel);
-//		add(authUsersBox);
-//		add(mapGroupsBox);
-//		add(remoteGroupToSystemAdminField);
-//		add(remoteGroupToRepoAdminField);
-//		add(remoteGroupToRepoCreatorField);
-//		add(groupsForPermissionManagementBox);
-////		add(prefixLabel);
-//		add(prefixField);
-////		add(options);
-//		add(mapGroupsPanel);
+		
+		form.add(listRolesview);
+
+		add(form);		
 	}
 
+	private class DropDownTranslationRenderer implements IChoiceRenderer<String> {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 4282172447769549480L;
+		
+		public DropDownTranslationRenderer() {
+		}
+
+		@Override
+		public Object getDisplayValue(String object) {
+			return getString("gb.realm.ugm.role."+object);
+		}
+
+		@Override
+		public String getIdValue(String object, int index) {
+			return object;
+		}
+	}
+	
 	/**
 	 * 
 	 */
--- src/main/java/com/gitblit/wicket/panels/UserServicesManagementPanel.java
+++ src/main/java/com/gitblit/wicket/panels/UserServicesManagementPanel.java
@@ -1,5 +1,10 @@
 package com.gitblit.wicket.panels;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.StatelessForm;
 
@@ -18,10 +23,45 @@ public class UserServicesManagementPanel extends BasePanel {
 			}
 		};
 		form.add(saveOrder);
-		for (String userService : userServices) {
-			form.add(new UserServiceConfigPanel(userService, userService));
+
+		Set<String> connectorParamsInternal = new HashSet<String>();
+		Set<String> connectorParamsLdap = new HashSet<String>();
+		Set<String> connectorParamsCrowd = new HashSet<String>();
+		Set<String> connectorParamsRedmine = new HashSet<String>();
+
+		connectorParamsLdap.add("realm.ldap.server");
+		connectorParamsLdap.add("realm.ldap.username");
+		connectorParamsLdap.add("realm.ldap.password");
+		connectorParamsLdap.add("realm.ldap.accountBase");
+		connectorParamsLdap.add("realm.ldap.groupMemberPattern");
+		connectorParamsLdap.add("realm.ldap.displayName");
+		connectorParamsLdap.add("realm.ldap.email");
+		connectorParamsLdap.add("realm.ldap.ldapCachePeriod");
+		connectorParamsLdap.add("realm.ldap.synchronizeUsers.enable");
+		connectorParamsLdap.add("realm.ldap.synchronizeUsers.removeDeleted");
+		connectorParamsLdap.add("realm.ldap.uid");
+
+		connectorParamsRedmine.add("realm.redmine.url");
+
+		connectorParamsCrowd.add("realm.crowd.url");
+		connectorParamsCrowd.add("realm.crowd.appname");
+		connectorParamsCrowd.add("realm.crowd.apppassword");
+
+		ArrayList<Set<String>> connectorParams = new ArrayList<Set<String>>();
+		connectorParams.add(connectorParamsInternal);
+		connectorParams.add(connectorParamsLdap);
+		connectorParams.add(connectorParamsCrowd);
+		connectorParams.add(connectorParamsRedmine);
+
+		for (int i = 0; i < userServices.length; i++) {
+			List<RoleMapping> dummyRoleMappings = new ArrayList<RoleMapping>();
+			dummyRoleMappings.add(new RoleMapping("dummyRole", RoleMapping.SYSTEM_USER));
+			form.add(new UserServiceConfigPanel(userServices[i],
+					userServicesNames[i], connectorParams.get(i), RoleMapping
+					.getSystemRoles(), dummyRoleMappings));
+			add(form);
 		}
-		add(form);		
+
 	}
 
 	/**
@@ -29,6 +69,9 @@ public class UserServicesManagementPanel extends BasePanel {
 	 */
 	private static final long serialVersionUID = 6037038749205815615L;
 	private static final String[] userServices = new String[] { "internal",
-			"ldap", "crowd", "redmine" };
+			"realm.ldap", "realm.crowd", "realm.redmine" };
+	private static final String[] userServicesNames = new String[] {
+			"Gitblit User and Group Management", "LDAP Directory",
+			"Atlassian Crowd", "Redmine" };
 
 }
