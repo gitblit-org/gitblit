bca8c5c52554b6aac65b8e2300675ae8f6af1d6d
James Moger <james.moger@gitblit.com>
2013-05-15 17:21:03 -0400

    Fix message escaping when combined with regex substitutions (issue 242)

--- src/main/java/com/gitblit/wicket/pages/RepositoryPage.java
+++ src/main/java/com/gitblit/wicket/pages/RepositoryPage.java
@@ -431,9 +431,9 @@ public abstract class RepositoryPage extends BasePage {
 	}
 
 	protected void addFullText(String wicketId, String text, boolean substituteRegex) {
-		String html = StringUtils.escapeForHtml(text, true);
+		String html = StringUtils.escapeForHtml(text, false);
 		if (substituteRegex) {
-			html = GitBlit.self().processCommitMessage(repositoryName, text);
+			html = GitBlit.self().processCommitMessage(repositoryName, html);
 		} else {
 			html = StringUtils.breakLinesForHtml(html);
 		}
