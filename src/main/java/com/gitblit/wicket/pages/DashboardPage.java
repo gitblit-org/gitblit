ada1a326fa36ceb46922006f87d4cff0c49e071f
James Moger <james.moger@gitblit.com>
2013-06-12 17:19:15 -0400

    Fixed double enumeration of repositories on dashboard page

--- src/main/java/com/gitblit/wicket/pages/DashboardPage.java
+++ src/main/java/com/gitblit/wicket/pages/DashboardPage.java
@@ -124,7 +124,7 @@ public class DashboardPage extends RootPage {
 		// parameters
 		int daysBack = params == null ? 0 : WicketUtils.getDaysBack(params);
 		if (daysBack < 1) {
-			daysBack = 14;
+			daysBack = 7;
 		}
 		Calendar c = Calendar.getInstance();
 		c.add(Calendar.DATE, -1*daysBack);
@@ -136,7 +136,7 @@ public class DashboardPage extends RootPage {
 		List<RepositoryModel> owned = new ArrayList<RepositoryModel>();
 		List<RepositoryModel> active = new ArrayList<RepositoryModel>();
 
-		for (RepositoryModel model : GitBlit.self().getRepositoryModels(user)) {
+		for (RepositoryModel model : getRepositoryModels()) {
 			if (model.isUsersPersonalRepository(user.username) || model.isOwner(user.username)) {
 				owned.add(model);
 			}
