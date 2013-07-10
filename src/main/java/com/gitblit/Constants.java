cb78b89ba770dc4edbd0f0a6ff70469cb8e0615c
James Moger <james.moger@gitblit.com>
2013-07-09 20:57:55 -0400

    Added External account type to enum

--- src/main/java/com/gitblit/Constants.java
+++ src/main/java/com/gitblit/Constants.java
@@ -480,7 +480,7 @@ public class Constants {
 	}
 	
 	public static enum AccountType {
-		LOCAL, LDAP, REDMINE, SALESFORCE, WINDOWS;
+		LOCAL, EXTERNAL, LDAP, REDMINE, SALESFORCE, WINDOWS;
 		
 		public boolean isLocal() {
 			return this == LOCAL;
