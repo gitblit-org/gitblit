5131d611ae044837318b97d206b6a89f2b25b4c4
James Moger <james.moger@gitblit.com>
2014-01-14 10:06:56 -0500

    Fixed incorrect tagger identity on dashboards (issue-276)

--- src/main/java/com/gitblit/utils/RefLogUtils.java
+++ src/main/java/com/gitblit/utils/RefLogUtils.java
@@ -642,7 +642,7 @@ public class RefLogUtils {
                             if (ref.getName().startsWith(Constants.R_TAGS)) {
                                 // treat tags as special events in the log
                                 if (!tags.containsKey(dateStr)) {
-                        			UserModel tagUser = newUserModelFrom(commit.getAuthorIdent());
+                        			UserModel tagUser = newUserModelFrom(ref.getAuthorIdent());
                         			Date tagDate = commit.getAuthorIdent().getWhen();
                         			tags.put(dateStr, new DailyLogEntry(repositoryName, tagDate, tagUser));
                                 }
@@ -653,7 +653,7 @@ public class RefLogUtils {
                             } else if (ref.getName().startsWith(Constants.R_PULL)) {
                                 // treat pull requests as special events in the log
                                 if (!pulls.containsKey(dateStr)) {
-                        			UserModel commitUser = newUserModelFrom(commit.getAuthorIdent());
+                        			UserModel commitUser = newUserModelFrom(ref.getAuthorIdent());
                         			Date commitDate = commit.getAuthorIdent().getWhen();
                         			pulls.put(dateStr, new DailyLogEntry(repositoryName, commitDate, commitUser));
                                 }
