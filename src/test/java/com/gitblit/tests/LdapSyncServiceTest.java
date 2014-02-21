a17ed65f281899cc8bc75453043e95728fb19686
Alfred Schmid <A.Schmid@ff-muenchen.de>
2014-02-21 11:52:11 +0100

    Added LdapSyncServiceTest to GitBlitSuite.

--- src/test/java/com/gitblit/tests/LdapSyncServiceTest.java
+++ src/test/java/com/gitblit/tests/LdapSyncServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.gitblit.service;
+package com.gitblit.tests;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -25,6 +25,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.gitblit.Keys;
+import com.gitblit.service.LdapSyncService;
 import com.gitblit.tests.mock.MemorySettings;
 
 /**
