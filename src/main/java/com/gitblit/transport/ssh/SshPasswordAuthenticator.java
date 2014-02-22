56abf9026b8a099b3465fdde008218b607c79806
James Moger <james.moger@gitblit.com>
2014-02-22 16:19:03 -0500

    Eliminate use of Dagger and do not create inner classes
    This change outlines somethings I'd like to see in your latest code.
    
    1. Elimination of Dagger
    2. Less noise from inner class instantiation
    3. Formalizing filesystem location for keys instead of Unix-y /tmp/
    4. Password authentication as alternative to Key authentication
    
    Change-Id: I7a76e1811ad7cd545444fd8b59bbe8c4f45ccfec

---
+++ src/main/java/com/gitblit/transport/ssh/SshPasswordAuthenticator.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 gitblit.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.gitblit.transport.ssh;
+
+import java.util.Locale;
+
+import org.apache.sshd.server.PasswordAuthenticator;
+import org.apache.sshd.server.session.ServerSession;
+
+import com.gitblit.manager.IGitblit;
+import com.gitblit.models.UserModel;
+
+/**
+ *
+ * @author James Moger
+ *
+ */
+public class SshPasswordAuthenticator implements PasswordAuthenticator {
+
+	protected final IGitblit gitblit;
+
+	public SshPasswordAuthenticator(IGitblit gitblit) {
+		this.gitblit = gitblit;
+	}
+
+	@Override
+	public boolean authenticate(String username, String password, ServerSession session) {
+		username = username.toLowerCase(Locale.US);
+		UserModel user = gitblit.authenticate(username, password.toCharArray());
+		if (user != null) {
+			SshSession sd = session.getAttribute(SshSession.KEY);
+			sd.authenticationSuccess(username);
+			return true;
+		}
+		return false;
+	}
+}
