55f9b66a4f5c6a982d663c544bb326810c125859
James Moger <james.moger@gitblit.com>
2014-03-14 14:55:38 -0400

    Rename authenticators for clarity

--- src/main/java/com/gitblit/transport/ssh/UsernamePasswordAuthenticator.java
+++ src/main/java/com/gitblit/transport/ssh/UsernamePasswordAuthenticator.java
@@ -15,11 +15,9 @@
  */
 package com.gitblit.transport.ssh;
 
-import java.security.PublicKey;
-import java.util.List;
 import java.util.Locale;
 
-import org.apache.sshd.server.PublickeyAuthenticator;
+import org.apache.sshd.server.PasswordAuthenticator;
 import org.apache.sshd.server.session.ServerSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,55 +27,35 @@ import com.gitblit.models.UserModel;
 
 /**
  *
- * @author Eric Myrhe
+ * @author James Moger
  *
  */
-public class SshKeyAuthenticator implements PublickeyAuthenticator {
+public class UsernamePasswordAuthenticator implements PasswordAuthenticator {
 
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	protected final IKeyManager keyManager;
-
 	protected final IAuthenticationManager authManager;
 
-	public SshKeyAuthenticator(IKeyManager keyManager, IAuthenticationManager authManager) {
-		this.keyManager = keyManager;
+	public UsernamePasswordAuthenticator(IAuthenticationManager authManager) {
 		this.authManager = authManager;
 	}
 
 	@Override
-	public boolean authenticate(String username, final PublicKey suppliedKey,
-			final ServerSession session) {
-		final SshDaemonClient client = session.getAttribute(SshDaemonClient.KEY);
-
+	public boolean authenticate(String username, String password, ServerSession session) {
+		SshDaemonClient client = session.getAttribute(SshDaemonClient.KEY);
 		if (client.getUser() != null) {
-			// TODO why do we re-authenticate?
 			log.info("{} has already authenticated!", username);
 			return true;
 		}
 
 		username = username.toLowerCase(Locale.US);
-		List<PublicKey> keys = keyManager.getKeys(username);
-		if (keys == null || keys.isEmpty()) {
-			log.info("{} has not added any public keys for ssh authentication", username);
-			return false;
+		UserModel user = authManager.authenticate(username, password.toCharArray());
+		if (user != null) {
+			client.setUser(user);
+			return true;
 		}
 
-		for (PublicKey key : keys) {
-			if (key.equals(suppliedKey)) {
-				UserModel user = authManager.authenticate(username, key);
-				if (user != null) {
-					client.setUser(user);
-					return true;
-				}
-			}
-		}
-
-		log.warn("could not authenticate {} for SSH using the supplied public key", username);
+		log.warn("could not authenticate {} for SSH using the supplied password", username);
 		return false;
-	}
-
-	public IKeyManager getKeyManager() {
-		return keyManager;
 	}
 }
