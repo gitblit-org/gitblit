4e6477c3d08e962fb2ff0c8f5c3cbbfe7f24fb39
David Ostrovsky <david@ostrovsky.org>
2014-03-18 08:33:42 +0100

    Expose SSH command as plugin extension point
    Example plugin is provided [1].
    
    [1] https://github.com/davido/gitblit-cookbook-plugin

---
+++ src/main/java/com/gitblit/transport/ssh/commands/PluginDispatchCommand.java
@@ -0,0 +1,11 @@
+package com.gitblit.transport.ssh.commands;
+
+import com.gitblit.models.UserModel;
+
+public class PluginDispatchCommand extends DispatchCommand {
+
+	@Override
+	protected void registerCommands(UserModel user) {
+		// no op
+	}
+}
