4d4dec5ee66a60d6489ba47c0d1f9bd532fdf454
David Ostrovsky <david@ostrovsky.org>
2014-03-19 21:43:41 +0100

    Report identified user and command on command failure
    Fix commented out code. Now failure is reported as:
    
      Internal server error (user admin) during gitblit version

--- src/main/java/com/gitblit/transport/ssh/gitblit/VersionCommand.java
+++ src/main/java/com/gitblit/transport/ssh/gitblit/VersionCommand.java
@@ -25,6 +25,9 @@ public class VersionCommand extends SshCommand {
 
   @Override
   public void run() {
+	  Object o = new Object();
+	  o = null;
+	  o.toString();
 	  stdout.println(Constants.getGitBlitVersion());
   }
 }
