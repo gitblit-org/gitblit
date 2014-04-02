fa58195758a4d8f18ead49f8ccb63d1ea6c341f0
James Moger <james.moger@gitblit.com>
2014-04-02 13:54:23 -0400

    Add hostname and ssh repo url methodso to SshCommand

--- src/main/java/com/gitblit/transport/ssh/commands/SshCommand.java
+++ src/main/java/com/gitblit/transport/ssh/commands/SshCommand.java
@@ -16,10 +16,18 @@ package com.gitblit.transport.ssh.commands;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.MessageFormat;
 
 import org.apache.sshd.server.Environment;
+import org.eclipse.jgit.util.SystemReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.gitblit.Keys;
+import com.gitblit.manager.IGitblit;
+import com.gitblit.utils.StringUtils;
 
 public abstract class SshCommand extends BaseCommand {
 
@@ -43,6 +51,36 @@ public abstract class SshCommand extends BaseCommand {
 				}
 			}
 		});
+	}
+
+	protected String getHostname() {
+		IGitblit gitblit = getContext().getGitblit();
+		String host = null;
+		String url = gitblit.getSettings().getString(Keys.web.canonicalUrl, "https://localhost:8443");
+		if (url != null) {
+			try {
+				host = new URL(url).getHost();
+			} catch (MalformedURLException e) {
+			}
+		}
+		if (StringUtils.isEmpty(host)) {
+			host = SystemReader.getInstance().getHostname();
+		}
+		return host;
+	}
+
+	protected String getRepositoryUrl(String repository) {
+		String username = getContext().getClient().getUsername();
+		String hostname = getHostname();
+		int port = getContext().getGitblit().getSettings().getInteger(Keys.git.sshPort, 0);
+		if (port == 22) {
+			// standard port
+			return MessageFormat.format("{0}@{1}/{2}.git", username, hostname, repository);
+		} else {
+			// non-standard port
+			return MessageFormat.format("ssh://{0}@{1}:{2,number,0}/{3}",
+					username, hostname, port, repository);
+		}
 	}
 
 	protected abstract void run() throws UnloggedFailure, Failure, Exception;
