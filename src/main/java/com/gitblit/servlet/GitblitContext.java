d316f8749213af7f25610ce6a7b8cb1391218fde
James Moger <james.moger@gitblit.com>
2014-12-03 14:42:10 -0500

    Use startManager(X) from startManager(Injector, Class)

--- src/main/java/com/gitblit/servlet/GitblitContext.java
+++ src/main/java/com/gitblit/servlet/GitblitContext.java
@@ -261,9 +261,7 @@ public class GitblitContext extends GuiceServletContextListener {
 	protected <X extends IManager> X startManager(Injector injector, Class<X> clazz) {
 		X x = loadManager(injector, clazz);
 		logManager(clazz);
-		x.start();
-		managers.add(x);
-		return x;
+		return startManager(x);
 	}
 
 	protected <X extends IManager> X startManager(X x) {
