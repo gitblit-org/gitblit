b32fa68832412374a1a905525a4e395d35b4d1ae
James Moger <james.moger@gitblit.com>
2016-04-20 10:06:09 -0400

    Use getInteger not getFilesize, fixes #1049

--- src/main/java/com/gitblit/manager/RepositoryManager.java
+++ src/main/java/com/gitblit/manager/RepositoryManager.java
@@ -1922,7 +1922,7 @@ public class RepositoryManager implements IRepositoryManager {
 		cfg.setPackedGitWindowSize(settings.getFilesize(Keys.git.packedGitWindowSize, cfg.getPackedGitWindowSize()));
 		cfg.setPackedGitLimit(settings.getFilesize(Keys.git.packedGitLimit, cfg.getPackedGitLimit()));
 		cfg.setDeltaBaseCacheLimit(settings.getFilesize(Keys.git.deltaBaseCacheLimit, cfg.getDeltaBaseCacheLimit()));
-		cfg.setPackedGitOpenFiles(settings.getFilesize(Keys.git.packedGitOpenFiles, cfg.getPackedGitOpenFiles()));
+		cfg.setPackedGitOpenFiles(settings.getInteger(Keys.git.packedGitOpenFiles, cfg.getPackedGitOpenFiles()));
 		cfg.setPackedGitMMAP(settings.getBoolean(Keys.git.packedGitMmap, cfg.isPackedGitMMAP()));
 
 		try {
