b291d08999b029721278b4f999a13b4ba1724638
paladox <paladox@users.noreply.github.com>
2017-03-18 17:54:24 +0000

    Fix sshd import part 5

--- src/main/java/com/gitblit/transport/ssh/SshServerSessionFactory.java
+++ src/main/java/com/gitblit/transport/ssh/SshServerSessionFactory.java
@@ -49,14 +49,14 @@ public class SshServerSessionFactory extends SessionFactory {
 			}
 		}
 
-		final SshServerSession session = (SshServerSession) super.createSession(io);
+		ServerSessionImpl session = super.createSession(io);
 		SocketAddress peer = io.getRemoteAddress();
 		SshDaemonClient client = new SshDaemonClient(peer);
 		session.setAttribute(SshDaemonClient.KEY, client);
 
 		// TODO(davido): Log a session close without authentication as a
 		// failure.
-		session.addCloseSessionListener(new SshFutureListener<CloseFuture>() {
+		session.addCloseFutureListener(new SshFutureListener<CloseFuture>() {
 			@Override
 			public void operationComplete(CloseFuture future) {
 				log.info("closed ssh session from {}", io.getRemoteAddress());
@@ -67,6 +67,6 @@ public class SshServerSessionFactory extends SessionFactory {
 
 	@Override
 	protected ServerSessionImpl doCreateSession(IoSession ioSession) throws Exception {
-		return new SshServerSession(getServer(), ioSession);
+		return new ServerSessionImpl(getServer(), ioSession);
 	}
 }
