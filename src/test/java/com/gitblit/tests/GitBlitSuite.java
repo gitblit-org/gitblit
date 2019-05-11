d1186a8add47abe6722400ed4c2c08fdef42529f
Tue Ton <chirontt@chirontt.org>
2019-05-11 09:41:31 -0400

    Zip all external repos to be part of the test suites locally.
    During the test run by GitBlitSuite test suite, some repos from GitHub
    were cloned and became part of the test data. These repos are now zipped
    to be part of gitblit repo itself, thus eliminating the network fetch at
    the start of test run which can be slow, especially with the JGit repo
    cloning which is huge and time consuming. The cloned JGit repo is now
    zipped and checked in to gitblit, along with the other 4 repos
    (hello-world, ambition, gitective and ticgit). They will be unzipped
    during the test suite run and be available in the local file system,
    thus avoiding the need for some network fetch.
    
    Special note on the zipped JGit repo: this repo is big (and growing all
    the time on GitHub), and takes up about 32MB of disk space after cloning
    from GitHub. I've made it smaller by resetting HEAD back to a commit of
    5 years ago (with git reset --hard <commitId> command), to put it back
    to roughly where/when the tests were written for it (which is not quite,
    because there are tons of commit history since which can't be removed.)
    This local JGit repo is then garbage-collected (with git gc --prune
    --aggressive) to reduce its size to about 19MB, then is zipped and
    checked in with this commit.

--- src/test/java/com/gitblit/tests/GitBlitSuite.java
+++ src/test/java/com/gitblit/tests/GitBlitSuite.java
@@ -16,11 +16,15 @@
 package com.gitblit.tests;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
@@ -80,9 +84,17 @@ public class GitBlitSuite {
 
 	public static final File USERSCONF = new File("src/test/config/test-users.conf");
 
-	public static final File HELLOWORLD_REPO_SOURCE = new File("src/test/data/hello-world.git");
+	private static final File AMBITION_REPO_SOURCE = new File("src/test/data/ambition.git");
 
-	public static final File HELLOWORLD_REPO_PROPERTIES = new File("src/test/data/hello-world.properties");
+	private static final File JGIT_REPO_SOURCE = new File("src/test/data/jgit.git");
+
+	private static final File TICGIT_REPO_SOURCE = new File("src/test/data/ticgit.git");
+
+	private static final File GITECTIVE_REPO_SOURCE = new File("src/test/data/gitective.git");
+
+	private static final File HELLOWORLD_REPO_SOURCE = new File("src/test/data/hello-world.git");
+
+	private static final File HELLOWORLD_REPO_PROPERTIES = new File("src/test/data/hello-world.properties");
 
 	public static final FileSettings helloworldSettings = new FileSettings(HELLOWORLD_REPO_PROPERTIES.getAbsolutePath());
 
@@ -191,12 +203,27 @@ public class GitBlitSuite {
 		startGitblit();
 
 		if (REPOSITORIES.exists() || REPOSITORIES.mkdirs()) {
+			if (!HELLOWORLD_REPO_SOURCE.exists()) {
+				unzipRepository(HELLOWORLD_REPO_SOURCE.getPath() + ".zip", HELLOWORLD_REPO_SOURCE.getParentFile());
+			}
+			if (!TICGIT_REPO_SOURCE.exists()) {
+				unzipRepository(TICGIT_REPO_SOURCE.getPath() + ".zip", TICGIT_REPO_SOURCE.getParentFile());
+			}
+			if (!JGIT_REPO_SOURCE.exists()) {
+				unzipRepository(JGIT_REPO_SOURCE.getPath() + ".zip", JGIT_REPO_SOURCE.getParentFile());
+			}
+			if (!AMBITION_REPO_SOURCE.exists()) {
+				unzipRepository(AMBITION_REPO_SOURCE.getPath() + ".zip", AMBITION_REPO_SOURCE.getParentFile());
+			}
+			if (!GITECTIVE_REPO_SOURCE.exists()) {
+				unzipRepository(GITECTIVE_REPO_SOURCE.getPath() + ".zip", GITECTIVE_REPO_SOURCE.getParentFile());
+			}
 			cloneOrFetch("helloworld.git", HELLOWORLD_REPO_SOURCE.getAbsolutePath());
-			cloneOrFetch("ticgit.git", "https://github.com/schacon/ticgit.git");
-			cloneOrFetch("test/jgit.git", "https://github.com/eclipse/jgit.git");
+			cloneOrFetch("ticgit.git", TICGIT_REPO_SOURCE.getAbsolutePath());
+			cloneOrFetch("test/jgit.git", JGIT_REPO_SOURCE.getAbsolutePath());
 			cloneOrFetch("test/helloworld.git", HELLOWORLD_REPO_SOURCE.getAbsolutePath());
-			cloneOrFetch("test/ambition.git", "https://github.com/defunkt/ambition.git");
-			cloneOrFetch("test/gitective.git", "https://github.com/kevinsawicki/gitective.git");
+			cloneOrFetch("test/ambition.git", AMBITION_REPO_SOURCE.getAbsolutePath());
+			cloneOrFetch("test/gitective.git", GITECTIVE_REPO_SOURCE.getAbsolutePath());
 
 			showRemoteBranches("ticgit.git");
 			automaticallyTagBranchTips("ticgit.git");
@@ -273,4 +300,47 @@ public class GitBlitSuite {
 			r.close();
 		}
 	}
+
+	private static void unzipRepository(String zippedRepo, File destDir) throws IOException {
+		System.out.print("Unzipping " + zippedRepo + "... ");
+		if (!destDir.exists()) {
+			destDir.mkdir();
+		}
+		byte[] buffer = new byte[1024];
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(zippedRepo));
+		ZipEntry zipEntry = zis.getNextEntry();
+		while (zipEntry != null) {
+			File newFile = newFile(destDir, zipEntry);
+			if (zipEntry.isDirectory()) {
+				newFile.mkdirs();
+			}
+			else {
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+				fos.close();
+			}
+			zipEntry = zis.getNextEntry();
+		}
+		zis.closeEntry();
+		zis.close();		
+		System.out.println("done.");
+	}
+
+	private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+		File destFile = new File(destinationDir, zipEntry.getName());
+
+		String destDirPath = destinationDir.getCanonicalPath();
+		String destFilePath = destFile.getCanonicalPath();
+		//guards against writing files to the file system outside of the target folder
+		//to prevent Zip Slip exploit
+		if (!destFilePath.startsWith(destDirPath + File.separator)) {
+			throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+		}
+
+		return destFile;
+	}
+
 }
