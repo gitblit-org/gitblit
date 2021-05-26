39cd663b2465f04bc13beca82be61e28fa8f8b7f
Zwixx <zwiixx@googlemail.com>
2021-05-26 17:05:26 +0200

    Modified Batch file for Windows, if it's called directly

--- src/main/distrib/win/gitblit-stop.cmd
+++ src/main/distrib/win/gitblit-stop.cmd
@@ -1,1 +1,5 @@
-@java -cp gitblit.jar;"%CD%\ext\*" com.gitblit.GitBlitServer --stop --baseFolder data %*
+@ECHO OFF
+
+SET CD=%~dp0
+SET CD=%CD:~0,-1%
+java -cp gitblit.jar;"%CD%\ext\*" com.gitblit.GitBlitServer --stop --baseFolder data %*
--- src/main/distrib/win/gitblit.cmd
+++ src/main/distrib/win/gitblit.cmd
@@ -1,1 +1,5 @@
-@java -cp gitblit.jar;"%CD%\ext\*" com.gitblit.GitBlitServer --baseFolder data %*
+@ECHO OFF
+
+SET CD=%~dp0
+SET CD=%CD:~0,-1%
+java -cp "%CD%\gitblit.jar";"%CD%\ext\*" com.gitblit.GitBlitServer --baseFolder data %*
