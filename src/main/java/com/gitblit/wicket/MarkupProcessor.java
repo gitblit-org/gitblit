gitblit-org/gitblit

commit b23269acc0f460f583311c679d751925b8402563

fix: Also parse exp links in MD pages
Add a link parser also for `ExpLinks` because we need to escape paths
to files in subfolders.
This closes #1358

src/main/java/com/gitblit/wicket/MarkupProcessor.java
@@ -47,6 +47,7 @@ import org.pegdown.LinkRenderer;
 import org.pegdown.ToHtmlSerializer;
 import org.pegdown.VerbatimSerializer;
 import org.pegdown.ast.ExpImageNode;
+import org.pegdown.ast.ExpLinkNode;
 import org.pegdown.ast.RefImageNode;
 import org.pegdown.ast.WikiLinkNode;
 import org.pegdown.plugins.ToHtmlSerializerPlugin;
@@ -355,6 +356,13 @@ public class MarkupProcessor {
 				String name = getDocumentName(path);
 				String url = getWicketUrl(DocPage.class, repositoryName, commitId, path);
 				return new Rendering(url, name);
+			}
+
+			@Override
+			public Rendering render(ExpLinkNode node, String text) {
+				String path = doc.getRelativePath(node.url);
+				String url = getWicketUrl(DocPage.class, repositoryName, commitId, path);
+				return new Rendering(url, text);
 			}
 		};
 

commit ebbd27be8c47579db837d4d174905c4ef1c6de39

Fixes external links broken in markdown rendering (#1392)
* This commit fixes what was broken in commit
https://github.com/gitblit/gitblit/commit/b23269acc0f460f583311c679d751925b8402563
due to #1358 issue

src/main/java/com/gitblit/wicket/MarkupProcessor.java
@@ -360,6 +360,22 @@ public class MarkupProcessor {
 
 			@Override
 			public Rendering render(ExpLinkNode node, String text) {
+				// Relative file-like MD links needs to be re-mapped to be relative to 
+				// repository name so that they display correctly sub-folder files
+				// Absolute links must be left un-touched.
+				
+				// Note: The absolute lack of comments in ExpLinkNode is... well...
+				// I assume, that getRelativePath is handling "file like" links
+				// like "/xx/tt"  or "../somefolder". What needs to be captured
+				// is a full URL link. The easiest is to ask java to parse URL
+				// and let it fail. Shame java.net.URL has no method to validate URL without
+				// throwing.
+				try {
+					new java.net.URL(node.url);
+					// This is URL, fallback to superclass.
+					return super.render(node,text);
+				} catch (java.net.MalformedURLException ignored) {};
+				// repository-relative link
 				String path = doc.getRelativePath(node.url);
 				String url = getWicketUrl(DocPage.class, repositoryName, commitId, path);
 				return new Rendering(url, text);