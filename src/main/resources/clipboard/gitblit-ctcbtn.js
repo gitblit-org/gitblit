be7d1b1beba706433a032425c48c44e63e54da63
Florian Zschocke <f.zschocke+git@gmail.com>
2022-11-21 01:11:52 +0100

    Add a "Copied" tooltip to the copy-to-clipboard button
    This is not the ideal version, since the height is too low for the tooltip
    used for the drop-down menus. Probably has something to do with the
    container or something. But at least something is there now, even if
    not the most beautiful.

---
+++ src/main/resources/clipboard/gitblit-ctcbtn.js
@@ -0,0 +1,72 @@
+// Instantiate the clipboarding
+var clipboard = new ClipboardJS('.ctcbtn');
+
+clipboard.on('success', function (e) {
+  showTooltip(e.trigger, 'Copied!');
+});
+
+clipboard.on('error', function (e) {
+  showTooltip(e.trigger, fallbackMessage(e.action));
+});
+
+// Attach events to buttons to clear tooltip again
+var btns = document.querySelectorAll('.ctcbtn');
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('mouseleave', clearTooltip);
+  btns[i].addEventListener('blur', clearTooltip);
+}
+
+// Show or hide tooltip by setting the tooltipped-active class
+// on a parent that containes tooltipped. Since the copy button
+// could be and image, or be hidden after clicking, the tooltipped
+// emelent might be higher in the hierarchy.
+var ttset
+function showTooltip(elem, msg) {
+  let ttelem = findTooltipped(elem);
+  if (ttelem != null) {
+    ttelem.classList.add('tooltipped-active');
+    ttelem.setAttribute('data-tt-text', msg);
+    ttset=Date.now();
+  }
+  else {
+    console.warn("Could not find any tooltipped element for clipboard button.", elem);
+  }
+}
+function clearTooltip(e) {
+  let ttelem = findTooltipped(e.currentTarget);
+  if (ttelem != null) {
+    let now = Date.now();
+    if (now - ttset < 500) {
+        // Give the tooltip some time to display
+        setTimeout(function(){ttelem.classList.remove('tooltipped-active')}, 1000)
+    }
+    else {
+        ttelem.classList.remove('tooltipped-active');
+//  e.currentTarget.removeAttribute('data-tt-text');
+    }
+  }
+  else {
+    console.warn("Could not find any tooltipped element for clipboard button.", e.currentTarget);
+  }
+}
+function findTooltipped(elem) {
+  do {
+    if (elem.classList.contains('tooltipped')) return elem;
+    elem = elem.parentElement;
+  } while (elem != null);
+  return null;
+}
+
+// If the API is not supported, at least fall back to a message saying
+// that now that the text is selected, Ctrl-C can be used.
+// This is still a problem in the repo URL dropdown. When it is hidden, Ctrl-C doesn't work.
+function fallbackMessage(action) {
+  var actionMsg = '';
+  if (/Mac/i.test(navigator.userAgent)) {
+    actionMsg = 'Press ⌘-C to copy';
+  }
+  else {
+    actionMsg = 'Press Ctrl-C to copy';
+  }
+  return actionMsg;
+}
